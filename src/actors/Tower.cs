7f286ca13691774d5836d7a27089124a0f343692
Dúcz Ákos <akos.ducz@gmail.com>
2024-12-29 19:17:54 +0100

    Add tower blueprint mode and layers

--- src/actors/Tower.cs
+++ src/actors/Tower.cs
@@ -7,6 +7,7 @@ namespace towerdefense;
 
 public class Tower : GameObject
 {
+    public bool isBluePrint = true;
     static Texture2D texture = Game1.contentManager.Load<Texture2D>("basic_mushroom");
     public Tower() : base("tower") {
         SetSprite(new Sprite(texture));
@@ -15,14 +16,25 @@ public class Tower : GameObject
     
     float elapsed = 0;
     float shotInterval = 100;
-    public override void Update(GameTime gameTime) {
-        elapsed += gameTime.ElapsedGameTime.Milliseconds;
 
+    private void Shoot(GameTime gameTime) {
         if (elapsed > shotInterval) {
             elapsed -= shotInterval;
             double rotation = gameTime.TotalGameTime.TotalMilliseconds/1000f;
             Projectile proj = new Projectile(new Vector2((float)Math.Sin(rotation),(float)Math.Cos(rotation)));
             proj.transform.localPosition = transform.localPosition;
+        }
+    }
+    public override void Update(GameTime gameTime) {
+        
+
+        elapsed += gameTime.ElapsedGameTime.Milliseconds;
+
+        if (isBluePrint) {
+            sprite.color = Color.Aqua * 0.5f;
+        } else {
+            sprite.color = Color.White;
+            Shoot(gameTime);
         }
 
         sprite.transform.localScale.Y = 1f + (float)Math.Sin(gameTime.TotalGameTime.TotalMilliseconds/700f)/50;
--- src/engine/Sprite.cs
+++ src/engine/Sprite.cs
@@ -14,6 +14,8 @@ public class Sprite : IDrawable
     private readonly Texture2D spriteTexture;
     public Color color = Color.White;
     public Transform transform = new Transform();
+
+    public float layer = 0f;
     
     public Sprite(Texture2D _texture) {
         spriteTexture = _texture;
@@ -27,6 +29,6 @@ public class Sprite : IDrawable
 
     public void Draw(GameTime gameTime, SpriteBatch spriteBatch) { 
         spriteBatch.Draw(spriteTexture, transform.GetPosition() - new Vector2(spriteTexture.Width, spriteTexture.Height)*transform.GetScale()/2, null,
-        color, 0f, Vector2.Zero, transform.GetScale(), SpriteEffects.None, 0f);
+        color, 0f, Vector2.Zero, transform.GetScale(), SpriteEffects.None, layer);
     }
 }
